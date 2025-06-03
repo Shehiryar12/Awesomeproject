4f771df0c156223e543d6e3ebac4d4046b71ec4c
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-06-03 20:58:51 +0500

    leran button.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -10,11 +10,29 @@
     }
 
     return(
-        <View>
-        <Text style= {{fontSize:30}} >Press on button</Text>
-        <Button title="PRESS" color="black" onPress={Press}></Button>
-        <Button title="Click" color="blue" onPress={()=>Press("I am fine")}></Button>
-      </View>
+    //     <View>
+    //     <Text style= {{fontSize:30}} >Press on button</Text>
+    //     <Button title="PRESS" color="black" onPress={Press}></Button>
+    //     <Button title="Click" color="blue" onPress={()=>Press("I am fine")}></Button>
+    //   </View>
+
+    <View>
+           <Text>Buttons</Text>  
+
+           <View style={{padding:20}}>
+              <Button title="PRESS"></Button>
+            </View> 
+
+            <View style={{padding:20,marginBottom: 20}}>
+              <Button title="PRESS" color="red"></Button>
+            </View>  
+
+            <View style={{padding:20,marginBottom: 20}}>
+              <Button title="PRESS" color="green"></Button>
+            </View>         
+    </View>
+
+
     );
       
  }
