65452bc1f5f4ca3d128948e6f05639b874abc937
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-06-10 11:50:08 +0500

    learn state.js

--- src/State.js
+++ src/State.js
@@ -1,48 +1,27 @@
-//  import React,{useState} from 'react'
-//  import{View,Text, Button, TextInput} from "react-native"
+ import React,{useState} from 'react'
+ import{View,Text, Button, TextInput} from "react-native"
 
 
-// export default function Hello(){
+export default function Hello(){
 
-// const [inputText, setInputText]=useState("");
-
-
-// const input=(type)=>{
-//      setInputText(type)
-// }
-// console.log(inputText)
+const [inputText, setInputText]=useState("abc");
 
 
-//     return(
+
+console.log(inputText)
+
+
+    return(
             
-//         <View>
-//             <Text>ali</Text>
+        <View>
+            <Text>ali</Text>
 
-//              <TextInput 
-
-//              onChangeText={r=>input(r)}
-//     />
-//         </View>
+           <Button title='change value' onPress={() => setInputText('def')}/>
+   
+        </View>
 
    
 
-//     );
-// };
-
- import React,{useState} from 'react'
- import{View,Text, Button} from "react-native"
-
-function Hello(){
-
-    const[count,setcount]=useState(3)
-    return(
-        <View>
-            <Text style={{fontSize:20}}>
-                 {count}
-            </Text>
-            <Button title='update' onPress={()=>setcount(count+1)} />    
-        </View>
     );
-}
+};
 
-export default Hello;
